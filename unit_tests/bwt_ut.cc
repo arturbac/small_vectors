5818fca3e7e84c0e65401d4d45f4e460b267dcbe
Artur Bać <artur@ebasoft.com.pl>
2024-01-10 11:14:04 +0100

    bwt

---
+++ unit_tests/bwt_ut.cc
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <algo/bwt.h>
+#include <unit_test_core.h>
+#include <coll/basic_fixed_string.h>
+
+using metatests::constexpr_test;
+using metatests::run_consteval_test;
+using metatests::run_constexpr_test;
+
+namespace ut = boost::ut;
+using ut::operator""_test;
+using namespace ut::operators::terse;
+using metatests::test_result;
+
+using value_type_list = metatests::type_list<char,char8_t,char16_t,char32_t,wchar_t>;
+// using value_type_list = metatests::type_list<char>;
+using coll::cast_fixed_string;
+using std::ranges::begin;
+
+namespace encode_test
+{
+static void do_test(test_result &result)
+  {
+  "encode"_test = [&]
+    {
+    auto fn_tmpl =
+      []<typename char_type>
+        ( char_type const *) -> metatests::test_result
+      {
+      using view_type = std::basic_string_view<char_type>;
+      std::array<char_type,128> buffer;
+        {
+        auto constexpr text{cast_fixed_string<char_type>("banana")};
+        auto constexpr expected{cast_fixed_string<char_type>("annb$aa")};
+        auto outit {algo::bwt::encode<'$'>( text, begin(buffer) )};
+        view_type v { begin(buffer), outit };
+        constexpr_test( v == expected);
+        }
+        {
+        auto constexpr text{cast_fixed_string<char_type>("abracadabra")};
+        auto constexpr expected{cast_fixed_string<char_type>("ard$rcaaaabb")};
+        auto outit {algo::bwt::encode<'$'>( text, begin(buffer) )};
+        view_type v { begin(buffer), outit };
+        constexpr_test( v == expected);
+        }
+      return {};
+      };
+
+    result |= run_consteval_test<value_type_list>(fn_tmpl);
+    result |= run_constexpr_test<value_type_list>(fn_tmpl);
+    };
+  }
+}
+namespace decode_test
+{
+static void do_test(test_result &result)
+  {
+  "encode"_test = [&]
+    {
+    auto fn_tmpl =
+      []<typename char_type>
+        ( char_type const *) -> metatests::test_result
+      {
+      using view_type = std::basic_string_view<char_type>;
+      std::array<char_type,128> buffer;
+        {
+        auto constexpr text{cast_fixed_string<char_type>("annb$aa")};
+        auto constexpr expected{cast_fixed_string<char_type>("banana")};
+        auto outit {algo::bwt::decode<'$'>( text, begin(buffer) )};
+        view_type v { begin(buffer), outit };
+        constexpr_test( v == expected);
+        }
+        {
+        auto constexpr text{cast_fixed_string<char_type>("ard$rcaaaabb")};
+        auto constexpr expected{cast_fixed_string<char_type>("abracadabra")};
+        auto outit {algo::bwt::decode<'$'>( text, begin(buffer) )};
+        view_type v { begin(buffer), outit };
+        constexpr_test( v == expected);
+        }
+      return {};
+      };
+
+    // result |= run_consteval_test<value_type_list>(fn_tmpl);
+    result |= run_constexpr_test<value_type_list>(fn_tmpl);
+    };
+  }
+}
+
+int main()
+{
+  test_result result;
+  encode_test::do_test(result);
+  decode_test::do_test(result);
+}
