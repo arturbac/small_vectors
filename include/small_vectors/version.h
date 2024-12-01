f60879d290d48d5c26e10470ede205fd787a557e
Artur Bać <artur@ebasoft.com.pl>
2024-12-01 22:45:02 +0100

    fix gcc build

--- include/small_vectors/version.h
+++ include/small_vectors/version.h
@@ -6,4 +6,7 @@
 #define small_vectors_clang_do_pragma(x) _Pragma(#x)
 #define small_vectors_clang_unsafe_buffer_usage_begin small_vectors_clang_do_pragma(clang unsafe_buffer_usage begin)
 #define small_vectors_clang_unsafe_buffer_usage_end small_vectors_clang_do_pragma(clang unsafe_buffer_usage end)
+#else
+#define small_vectors_clang_unsafe_buffer_usage_begin
+#define small_vectors_clang_unsafe_buffer_usage_end
 #endif
